3f4b741555c00324c6dd1316f485ef12e039daa9
Sorikairo <sorikairox@gmail.com>
2019-12-26 16:28:18 +0100

    add install and build scripts

---
+++ buid-all.sh
@@ -0,0 +1,3 @@
+cd background && yarn build
+cd ../desktop && yarn build
+cd ../in-game && yarn build
---
+++ install.sh
@@ -0,0 +1,3 @@
+cd background && yarn install
+cd ../desktop && yarn install
+cd ../in-game && yarn install
